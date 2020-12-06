9a23e7bcec0a4495d9e5d8e113c283f52f3e1834
Rocio Batres <jonasgirl21@gmail.com>
2020-12-06 06:23:50 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_060108) do
-
-  create_table "movie", force: :cascade do |t|
-    t.string "title"
-    t.integer "release_date"
-    t.string "director"
-    t.string "lead"
-    t.boolean "in_theaters"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

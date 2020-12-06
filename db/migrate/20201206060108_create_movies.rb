50239555825e544a06a1f4936e62294c6322a33a
Rocio Batres <jonasgirl21@gmail.com>
2020-12-06 06:11:00 +0000

    Automatically backed up by Learn

--- db/migrate/20201206060108_create_movies.rb
+++ db/migrate/20201206060108_create_movies.rb
@@ -1,9 +1,9 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :title :string 
-    add_column :release_date :integer 
-    add_column :director :string 
-    add_column :lead :string 
-    add_column :in_theaters :boolean 
+    add_column :title, :string 
+    add_column :release_date, :integer 
+    add_column :director, :string 
+    add_column :lead, :string 
+    add_column :in_theaters, :boolean 
   end
 end
